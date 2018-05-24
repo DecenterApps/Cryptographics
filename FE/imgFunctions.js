72b4298544bf49af9ce6ca845935aaae66277689
madjarevicn <nikola.madjarevic@gmail.com>
2018-05-24 23:04:36 +0200

    Calculate random seed - web3

--- FE/imgFunctions.js
+++ FE/imgFunctions.js
@@ -11,7 +11,27 @@ function calculateSeed(random_seed,iterations){
         leftPad((iterations).toString(16), 64, 0),
         {encoding : "hex"});
 
-    return util.bufferToHex(hash);
+    return hash;
 }
 
-console.log(calculateSeed(123123,5));
+
+function calculateNthSeed(random_seed) {
+    var seed = calculateSeed(random_seed,1);
+    console.log(seed);
+    seed = seed.toString().substr(2);
+    var x = leftPad((2).toString(16), 64, 0);
+    var secondSeed = web3.sha3( seed+x, {encoding: "hex"});
+    console.log(secondSeed);
+}
+function calculateFinalSeed(random_seed, iterations){
+    var hash = calculateSeed(random_seed,iterations);
+    console.log(hash);
+    var x = leftPad((2).toString(16), 64, 0);
+    console.log(x);
+    hash = hash.toString().substr(2);
+    var h1 = web3.sha3(hash + x, {encoding : "hex"});
+    console.log(h1);
+}
+
+calculateNthSeed(123123);
+calculateFinalSeed(123123,1);
