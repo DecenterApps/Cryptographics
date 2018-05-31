84a3268576112557eb650106fa6dea07a9b42c66
madjarevicn <nikola.madjarevic@gmail.com>
2018-05-31 11:03:22 +0200

    Minor fix

--- view/script.js
+++ view/script.js
@@ -1,20 +1,31 @@
 const functions = require('../scripts/imgFunctions');
-
+const utils = require('../scripts/utils');
 
 let randomSeed = 123123;
 let iterations = 5;
-let potentialAssets = ["0x0000000000000000000001000002000003000004000005000006000007000008",
-    "0x0000000000000000000001000002000003000004000005000006000007000008"];
+let potentialAssets = [1,2,3,4,5,6,7,8,9,10,11];
+potentialAssets = utils.encode(potentialAssets);
+
+
+
+function merge_objects(obj1,obj2){
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+}
+
 
 async function getData(randomSeed, iterations, potentialAssets) {
     var assets = functions.getImage(randomSeed, iterations, potentialAssets);
-    var assetStats = [];
+    var allDataAboutAsset = [];
     for(let i=0; i<assets.length; i++){
         let stats = await functions.getAssetStats(assets[i].id);
-        assetStats.push(stats);
-        console.log(assets[i]);
-        console.log(assetStats[i]);
+        let final = merge_objects(assets[i], stats);
+        allDataAboutAsset.push(final);
+        console.log(final);
     }
 }
+
 
 getData(randomSeed,iterations,potentialAssets);
