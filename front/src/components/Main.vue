3656e05e156196fb88055e00bc7636d7da43b1ad
madjarevicn <nikola.madjarevic@gmail.com>
2018-06-21 12:33:16 +0200

    Add routing

--- front/src/components/Main.vue
+++ front/src/components/Main.vue
@@ -2,7 +2,8 @@
     <div>
         <my-header></my-header>
         <h2> Cryptographics </h2>
-        <parent></parent>
+        <about v-if="currentRoute=='/about'"></about>
+        <parent v-if="currentRoute=='/galery'"></parent>
         <my-footer></my-footer>
     </div>
 </template>
@@ -11,13 +12,32 @@
     import Header from './Header.vue';
     import Footer from './Footer.vue';
     import Parent from './Parent.vue';
+    import About from './About.vue';
+
+
+    const routes = {
+        '/' : 'Home',
+        '/about' : About,
+        '/create' : 'CreateImage',
+        '/gallery' : 'Gallery',
+        '/assetPacks' : 'AssetPacks',
+    }
     export default {
+        data : {
+            currentRoute: window.location.pathname,
+        },
 
         components: {
             'my-header' : Header,
             'my-footer' : Footer,
             'parent' : Parent,
+            'about' : About,
         },
+
+        beforeCreate() {
+            this.currentRoute = window.location.pathname;
+            console.log("ROUTE" + this.currentRoute);
+        }
     }
 </script>
 
