1a009c66492ab55e3ef97461e5b87e3c3136f87d
Mateja Trifunovski <mtrifunovski@gmail.com>
2018-06-26 18:33:46 +0200

    Add social links to footer

--- front/src/components/Footer/Footer.vue
+++ front/src/components/Footer/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="footer">
         <div class="footer-container">
             <div class="logo">
-                <img src="../../assets/logo.png" alt="">
+                <img src="../../assets/logo.png" alt=""> Cryptographics
             </div>
             <div class="links-section">
                 <router-link to="/gallery"> Gallery</router-link>
@@ -10,7 +10,10 @@
                 <router-link to="/about">About</router-link>
             </div>
             <div class="social">
-
+                <img src="./assets/facebook.png" alt="">
+                <img src="./assets/instagram.png" alt="">
+                <img src="./assets/twitter.png" alt="">
+                <img src="./assets/youtube.png" alt="">
             </div>
         </div>
     </footer>
@@ -35,12 +38,23 @@
             text-decoration: none;
         }
 
+
         .footer-container {
             width: 1120px;
             margin: 0 auto;
             padding: 15px 0;
             display: flex;
             justify-content: space-between;
+
+            .logo {
+                display: flex;
+                align-items: center;
+                color: #fff;
+                font-size: 15px;
+                img {
+                    margin-right: 15px;
+                }
+            }
 
             .links-section {
                 display: flex;
@@ -53,6 +67,14 @@
                     &.router-link-active {
                         text-decoration: underline;
                     }
+                }
+            }
+
+            .social {
+                display: flex;
+                align-items: center;
+                img {
+                    margin-right: 20px;
                 }
             }
         }
