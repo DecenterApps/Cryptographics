b773e33065b9390083bf27d086b807fa728bf47e
Mateja Trifunovski <mtrifunovski@gmail.com>
2018-06-29 12:19:39 +0200

    Change allAssets page

--- front/src/components/AllAssets.vue
+++ front/src/components/AllAssets.vue
@@ -1,58 +1,61 @@
 <template>
     <div>
-        <div v-for="(asset,key) in allAssetPaths">
-            <label> {{key}}</label>
-            <img :src=asset>
+        <slider-gallery></slider-gallery>
+
+        <div class="container">
+            <div class="asset-packs-wrapper">
+                <div v-for="(asset,key) in allAssetPaths">
+                    <label> {{key}}</label>
+                    <img :src=asset>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
+  import SliderGallery from './SliderGallery/SliderGallery.vue';
+  import MyAssets from './MyAssets.vue';
 
-        data: () => ({
-            allAssetPaths: [],
-        }),
+  export default {
+    components: { MyAssets, SliderGallery },
+    data: () => ({
+      allAssetPaths: [],
+    }),
 
-        async created(){
-            let assetDir = "http://localhost:3000/dist/assets/";
-            let len = 50;
-            let images =[];
-            for(let i=1; i<len; i++){
-                if(i<10){
-                    let image = assetDir+"0"+i.toString()+".png";
-                    images.push(image);
-                } else {
-                    let image = assetDir+i.toString()+".png";
-                    images.push(image);
-                }
-            }
-            this.allAssetPaths = images;
-        },
-        computed: {
+    async created() {
+      let assetDir = 'http://localhost:3000/dist/assets/';
+      let len = 50;
+      let images = [];
+      for (let i = 1; i < len; i++) {
+        if (i < 10) {
+          let image = assetDir + '0' + i.toString() + '.png';
+          images.push(image);
+        } else {
+          let image = assetDir + i.toString() + '.png';
+          images.push(image);
+        }
+      }
+      this.allAssetPaths = images;
+    },
+    computed: {},
+    methods: {},
+    render() {
 
-        },
-        methods: {
-
-        },
-        render() {
-
-        },
-    }
+    },
+  };
 </script>
 
 <style scoped>
-    img{
-        margin-right: 100px;
+    img {
+        margin-right: 20px;
         margin-top: 20px;
         margin-bottom: 20px;
         width: 150px;
         height: 150px;
     }
-
-
-    div{
-        display: inline-block;
+    .asset-packs-wrapper {
+        display: flex;
+        flex-wrap: wrap;
     }
-
 </style>
