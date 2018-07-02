07aa8c6c16a4d621025039b2202cd076e664a6fe
madjarevicn <nikola.madjarevic@gmail.com>
2018-07-02 10:55:39 +0200

    Log price

--- front/src/components/CreateArt/ArtBuilder/ArtBuilder.vue
+++ front/src/components/CreateArt/ArtBuilder/ArtBuilder.vue
@@ -81,6 +81,7 @@
                 console.log('ITERATIONS: ' + this.iterations);
                 console.log('POTENTIAL ASSETS: ' + pot);
                 console.log('MM ACCOUNT: ' + this.metamask_account);
+                console.log('IMAGE PRICE: ' + this.image_price);
                 let img = await methods.createImage(this.random_hash_ids, this.timestamp, this.iterations -1, pot, 'Madjar', this.metamask_account, this.image_price, ipfsHash);
                 console.log(img);
                 // this.timestamp = new Date().getTime();
