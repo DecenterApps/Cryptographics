7a9909345bca4f895cdda31c0aa3b3be70f385cc
madjarevicn <nikola.madjarevic@gmail.com>
2018-07-03 15:46:16 +0200

    Add UploadAssetPack page

---
+++ front/src/components/UploadPack/UploadAssetPacks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1> Upload asset pack</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "upload-asset-packs"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- front/src/routes.js
+++ front/src/routes.js
@@ -4,13 +4,14 @@ import Home from './components/Home/Home.vue';
 import AllAssets from './components/AllAssets.vue'
 import Profile from './components/MyProfile.vue';
 import CreateArt from './components/CreateArt/CreateArt.vue';
-
+import UploadAssetPacks from './components/UploadPack/UploadAssetPacks.vue';
 export default [
   { path: '/', component: Home },
   { path: '/legacy', component: Parent },
   { path: '/about', component: About },
   { path: '/create', component: CreateArt },
   // { path: '/gallery', component: About },
+  { path: '/upload', component: UploadAssetPacks},
   { path: '/asset_packs', component: AllAssets },
   { path: '/profile', component: Profile },
 ];
