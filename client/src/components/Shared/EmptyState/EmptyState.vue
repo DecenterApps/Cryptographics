f4193f909de55a1d31ac3b1682910ec8e1cfe1a5
astankovic <aleksa.stankovic@berkeley.edu>
2018-09-24 14:46:43 +0200

    Add empty state component. Implement it with the gallery page.

---
+++ client/src/components/Shared/EmptyState/EmptyState.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="empty-state">
+        <div v-if="type === 'all'">There are no Cryptographics created at the moment.</div>
+        <div v-if="type === 'sale'">There are no Cryptographics for sale at the moment.</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'EmptyState',
+    props: {
+      type: {
+        type: String,
+      }
+    },
+  };
+</script>
+
+<style scoped>
+    .empty-state {
+        margin: 20px 0;
+        font-size: 32px;
+        line-height: 52px;
+        font-family: 'YoungSerif-Regular', serif;
+    }
+</style>
