9ce8e074c061c4cef2817875511250ca62eeca3d
astankovic <aleksa.stankovic@berkeley.edu>
2018-09-27 12:23:50 +0200

    Change wording for user edit profile picture input.

--- client/src/components/Shared/EditProfile/EditProfile.vue
+++ client/src/components/Shared/EditProfile/EditProfile.vue
@@ -32,7 +32,7 @@
                         button-style="secondary"
                         :multiple="false"
                         @change="onFileChanged">
-                        <span v-if="newAvatarBytes32 === ''">Profile image aspect ratio needs to be 1:1.</span>
+                        <span v-if="newAvatarBytes32 === ''">Profile image aspect ratio should be 1:1.</span>
                         <span v-else>You have selected: {{ imageName.slice(0, 22) }}<span v-if="imageName.length > 22">...</span></span>
                     </input-file>
                 </div>
