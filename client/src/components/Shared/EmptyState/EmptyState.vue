abcc01367c9fb292d29e9e86fdf23a1071f2e746
astankovic <aleksa.stankovic@berkeley.edu>
2018-10-02 09:49:35 +0200

    Add appropriate messages for empty states and call to actions om empty states.

--- client/src/components/Shared/EmptyState/EmptyState.vue
+++ client/src/components/Shared/EmptyState/EmptyState.vue
@@ -1,33 +1,29 @@
 <template>
     <div class="empty-state">
-        <div v-if="type === 'gallery-all'">There are no Cryptographics created yet.</div>
-        <div v-if="type === 'gallery-sale'">There are no Cryptographics for sale yet.</div>
-
-        <div v-if="type === 'profile-gallery-all-own'">You don't own any Cryptographics yet.</div>
-        <div v-if="type === 'profile-gallery-all'">This user doesn't own any Cryptographics yet.</div>
-
-        <div v-if="type === 'profile-gallery-created-own'">
-            You haven't created any Cryptographics yet. <router-link to="/create-cryptographic">Start creating!</router-link>
-        </div>
-        <div v-if="type === 'profile-gallery-created'">This hasn't created any Cryptographics yet.</div>
-
-        <div v-if="type === 'profile-gallery-bought-own'">
-            You haven't bought any Cryptographics yet. Check some out in the <router-link to="/gallery">gallery</router-link>.
-        </div>
-        <div v-if="type === 'profile-gallery-bought'">This user hasn't bought any Cryptographics yet.</div>
-
-        <div v-if="type === 'profile-asset-packs-all-own'">You don't own any asset packs yet.</div>
-        <div v-if="type === 'profile-asset-packs-all'">This user doesn't own any asset packs yet.</div>
-        <div v-if="type === 'profile-asset-packs-bought-own'">
-            You haven't bought any asset packs yet. Take a look at the <router-link to="/asset-packs">market</router-link>.
-        </div>
-        <div v-if="type === 'profile-asset-packs-bought'">This user hasn't bought any asset packs yet. </div>
-        <div v-if="type === 'profile-asset-packs-created-own' || type === 'asset-packs-created'">
-            You haven't created any asset packs yet. <router-link to="/create-asset-pack">Create one now!</router-link>
-        </div>
-        <div v-if="type === 'profile-asset-packs-created'">This user hasn't created any asset packs yet.</div>
+        <div v-if="type === 'gallery-all'">There are no cryptographics created yet.</div>
+        <div v-if="type === 'gallery-sale'">There are no cryptographics for sale yet.</div>
 
         <div v-if="type === 'asset-packs-all'">There are no asset packs created yet.</div>
+
+        <!--Your profile cryptographic gallery-->
+        <div v-if="type === 'profile-gallery-all-own'">You don't own any cryptographics yet. Try <router-link to="/create-cryptographic">creating</router-link> one now!</div>
+        <div v-if="type === 'profile-gallery-created-own'">You haven't created any cryptographics yet. <router-link to="/create-cryptographic">Start creating!</router-link></div>
+        <div v-if="type === 'profile-gallery-bought-own'">You haven't bought any cryptographics yet. Check some out in the <router-link to="/gallery">gallery</router-link>.</div>
+
+        <!--Other users profile cryptographic gallery-->
+        <div v-if="type === 'profile-gallery-all'">This user doesn't own any cryptographics yet.</div>
+        <div v-if="type === 'profile-gallery-created'">This user hasn't created any cryptographics yet.</div>
+        <div v-if="type === 'profile-gallery-bought'">This user hasn't bought any cryptographics yet.</div>
+
+        <!--Your profile asset packs-->
+        <div v-if="type === 'profile-asset-packs-all-own'">You don't own any asset packs yet. Check out the collection <router-link to="/asset-packs">here</router-link>.</div>
+        <div v-if="type === 'profile-asset-packs-bought-own'">You haven't bought any asset packs yet. Take a look at the <router-link to="/asset-packs">market</router-link>.</div>
+        <div v-if="type === 'profile-asset-packs-created-own' || type === 'asset-packs-created'">You haven't created any asset packs yet. <router-link to="/create-asset-pack">Create one now!</router-link></div>
+
+        <!--Other users profile asset packs-->
+        <div v-if="type === 'profile-asset-packs-all'">This user doesn't own any asset packs yet.</div>
+        <div v-if="type === 'profile-asset-packs-bought'">This user hasn't bought any asset packs yet. </div>
+        <div v-if="type === 'profile-asset-packs-created'">This user hasn't created any asset packs yet.</div>
     </div>
 </template>
 
